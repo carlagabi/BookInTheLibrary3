d4c97ae917aed13be8356ae92b2597ba08fc901e
carlagabi <carlagabi.oliveirapardo@gmail.com>
2023-03-08 22:10:57 -0300

    Intent e notificação

--- app/src/main/java/com/example/bookinthelibrary/SecondFragment.java
+++ app/src/main/java/com/example/bookinthelibrary/SecondFragment.java
@@ -1,11 +1,19 @@
 package com.example.bookinthelibrary;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -29,6 +37,55 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+
+                Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent =
+                        PendingIntent.getActivity(
+                                getActivity().getApplicationContext(),
+                                0, intent, 0);
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(getActivity().getApplicationContext());
+                String NOTIFICATION_CHANEL_ID = "my_chanel_id_01";
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel notificationChannel = new
+                            NotificationChannel(
+                            NOTIFICATION_CHANEL_ID,
+                            "my notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+                    notificationChannel.setDescription(
+                            "Description");
+                    notificationChannel.setLightColor(
+                            Color.RED);
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setVibrationPattern(
+                            new long[]{0,1000,500,1000});
+                    notificationChannel.enableVibration(true);
+                    notificationManager.createNotificationChannel(
+                            notificationChannel);
+
+                    NotificationCompat.Builder builder = new
+                            NotificationCompat.Builder(
+                            getActivity().getApplicationContext(),
+                            NOTIFICATION_CHANEL_ID)
+                            .setContentTitle("Minha Notificação")
+                            .setContentText("Minha primeira notificação")
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true);
+
+                    notificationManager.notify(0001, builder.build());
+
+                }
+            }
+        });
     }
 
     @Override
